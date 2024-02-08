40f9c6adbbf6d93b2da6e2c16c41bb34609b7f68
Talal Ahmed <talalsidd31@gmail.com>
2024-02-08 05:29:50 +0500

    list mapping done

---
+++ components/List.js
@@ -0,0 +1,11 @@
+import React from "react";
+import Item from "./Item";
+
+const List = ({ arr }) => {
+  const items = arr.map((item) => {
+    return <Item text={item} />;
+  });
+  return <ul>{items}</ul>;
+};
+
+export default List;
--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,16 @@
 import React from 'react'
 import Greet from '@/components/greet'
+import Button from '@/components/Button'
+import List from '@/components/List'
 
 const index = () => {
+  const arr = ["This is an item", "This is an item 2", "This is an item 3"]
   return (
     <div>
       <Greet/>
+
+      <List arr={arr}/>
+      <Button text="Add"/>
     </div>
   )
 }
