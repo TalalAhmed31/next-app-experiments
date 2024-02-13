ae7d65bcd6b5c02a361c8c8ab0ff8576a2504a45
Talal Ahmed <talalsidd31@gmail.com>
2024-02-13 23:35:12 +0500

    tests

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,11 @@
 import React from "react";
-import Greet from "@/components/greet";
+import Greet from "@/components/Greet";
 import Button from "@/components/Button";
 import List from "@/components/List";
 import { useState } from "react";
 
 const index = () => {
-  const [arr, setArr] = useState(["This is an item", "This is an item 2", "This is an item 3"]);
+  const [arr, setArr] = useState(["This is an item", "This is an item 2", "This is an item 3", "..."]);
 
   const buttonClick = () => {
     setArr([...arr , "New Item"]);
